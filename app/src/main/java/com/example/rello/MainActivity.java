9dfff179d8a59c1d260637ef3460a22a8f289822
Sidney Shane Dizon <sidneyshane_dizon@yahoo.com>
2019-11-28 21:56:37 -0700

    created working: splash login

--- app/src/main/java/com/example/rello/MainActivity.java
+++ app/src/main/java/com/example/rello/MainActivity.java
@@ -2,16 +2,44 @@ package com.example.rello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.rello.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Btn_Create, Btn_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Btn_Create = findViewById(R.id.create_account);
+        Btn_Login = findViewById(R.id.log_in);
+        Btn_Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAccountCreation();
+            }
+        });
+        Btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
     }
+    public void openAccountCreation(){
+        Intent intent = new Intent(this, create_account_Activity.class);
+        startActivity(intent);
+    }
+    public void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/rello/create_account_Activity.java
@@ -0,0 +1,14 @@
+package com.example.rello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_account_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_);
+    }
+}
