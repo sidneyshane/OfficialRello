d417d6bc0ec9b84114c4616df66d137498bf1077
Sidney Shane Dizon <sidneyshane_dizon@yahoo.com>
2019-11-29 03:06:02 -0700

    created working: group, event, chat, calendar

--- app/src/main/java/com/example/rello/chats_Activity.java
+++ app/src/main/java/com/example/rello/chats_Activity.java
@@ -2,13 +2,65 @@ package com.example.rello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class chats_Activity extends AppCompatActivity {
+
+    ImageButton Btn_event, Btn_group, Btn_chat, Btn_calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats_);
+
+        Btn_event = findViewById(R.id.events_btn);
+        Btn_group = findViewById(R.id.groups_btn);
+        Btn_chat = findViewById(R.id.chats_btn);
+        Btn_calendar = findViewById(R.id.calendar_btn);
+
+        Btn_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEvents();
+            }
+        });
+        Btn_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGroups();
+            }
+        });
+        Btn_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openChats();
+            }
+        });
+        Btn_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCalendar();
+            }
+        });
+    }
+
+    public void openEvents(){
+        Intent intent = new Intent(this, eventsActivity.class);
+        startActivity(intent);
+    }
+    public void openGroups(){
+        Intent intent = new Intent(this, groups_Activity.class);
+        startActivity(intent);
+    }
+    public void openChats(){
+        Intent intent = new Intent(this, chats_Activity.class);
+        startActivity(intent);
+    }
+    public void openCalendar(){
+        Intent intent = new Intent(this, calendar_Activity.class);
+        startActivity(intent);
     }
 }
