dea454aeb4ce749041dc9877baf514903e8c924e
Sidney Shane Dizon <sidneyshane_dizon@yahoo.com>
2019-11-29 00:25:31 -0700

    created working: onboarding

--- app/src/main/java/com/example/rello/onboarding_3_Activity.java
+++ app/src/main/java/com/example/rello/onboarding_3_Activity.java
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class onboarding_1_Activity extends AppCompatActivity {
+public class onboarding_3_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding_1_);
+        setContentView(R.layout.activity_onboarding_3_);
         Thread myThread = new Thread(){
             @Override
             public void run() {
                 try {
                     sleep(5000);
-                    Intent intent = new Intent(getApplicationContext(), onboarding_2_Activity.class);
+                    Intent intent = new Intent(getApplicationContext(), onboarding_3_Activity.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
---
+++ app/src/main/java/com/example/rello/onboarding_4_Activity.java
@@ -0,0 +1,21 @@
+package com.example.rello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class onboarding_4_Activity extends AppCompatActivity {
+
+    Button Btn_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_4_);
+
+
+
+    }
+
+}
