881a4c1665765d25dd61c4104e2e3c31701ff7b2
Dayan <dayanjayasuriya7@gmail.com>
2019-12-03 01:37:59 -0700

    Login Done

--- app/src/main/java/com/example/rello/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/rello/ui/login/LoginActivity.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -24,16 +25,22 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.rello.home_Activity;
 import com.example.rello.onboarding_1_Activity;
 import com.example.rello.create_account_Activity;
 import com.example.rello.R;
 import com.example.rello.ui.login.LoginViewModel;
 import com.example.rello.ui.login.LoginViewModelFactory;
+import com.sendbird.android.SendBird;
+import com.sendbird.android.SendBirdException;
+import com.sendbird.android.User;
 
 public class LoginActivity extends AppCompatActivity {
-
+    final static String APP_ID = "243BA639-D510-4A48-9834-CF82B1ABB4E9";
     private LoginViewModel loginViewModel;
-    Button Btn_Signup;
+    private Button Btn_Signup;
+    private Button btnLogin;
+    private SharedPreferences mPrefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,86 +48,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
-
+        mPrefs = getSharedPreferences("label", 0);
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        btnLogin = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
+        SendBird.init(APP_ID, this.getApplicationContext());
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                String userId = usernameEditText.getText().toString();
+                userId = userId.replaceAll("\\s", "");
 
+                String password = passwordEditText.getText().toString();
+                SharedPreferences.Editor mEditor = mPrefs.edit();
+                mEditor.putString("userId", userId).commit();
+                mEditor.putString("password", password).commit();
+                connectToSendBird(userId, password);
             }
         });
 
@@ -129,6 +75,63 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openSignUp();
+            }
+        });
+    }
+
+    /**
+     * Attempts to connect a user to SendBird.
+     * @param userId The unique ID of the user.
+     * @param userNickname The user's nickname, which will be displayed in chats.
+     */
+    private void connectToSendBird(final String userId, final String userNickname) {
+        btnLogin.setEnabled(false);
+
+        SendBird.connect(userId, new SendBird.ConnectHandler() {
+            @Override
+            public void onConnected(User user, SendBirdException e) {
+                if (e != null) {
+                    // Error!
+                    Toast.makeText(
+                            LoginActivity.this, "" + e.getCode() + ": " + e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+
+                    // Show login failure snackbar
+                    btnLogin.setEnabled(true);
+                    return;
+                }
+
+                // Update the user's nickname
+                updateCurrentUserInfo(userNickname);
+
+                Intent intent = new Intent(LoginActivity.this, home_Activity.class);
+                intent.putExtra("userID", userId);
+                intent.putExtra("userNickname", userNickname);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Updates the user's nickname.
+     * @param userNickname The new nickname of the user.
+     */
+    private void updateCurrentUserInfo(String userNickname) {
+        SendBird.updateCurrentUserInfo(userNickname, null, new SendBird.UserInfoUpdateHandler() {
+            @Override
+            public void onUpdated(SendBirdException e) {
+                if (e != null) {
+                    // Error!
+                    Toast.makeText(
+                            LoginActivity.this, "" + e.getCode() + ":" + e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+
+                    return;
+                }
+
             }
         });
     }
