92b838d3ea157d94ef08bdd0c3cff480cb7e2b3e
Dayan <dayanjayasuriya7@gmail.com>
2019-12-05 04:10:55 -0700

    Event Creation done

---
+++ app/src/main/java/com/example/rello/Event.java
@@ -0,0 +1,51 @@
+package com.example.rello;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+
+public class Event {
+    private String name;
+    private String description;
+    private boolean hiddenFlag;
+    private String eventDate;
+
+    Event(String aName, String aDate, String aDesc, boolean aFlag) {
+        setName(aName);
+        setEventDate(aDate);
+        setDescription(aDesc);
+        setHiddenFlag(aFlag);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isHiddenFlag() {
+        return hiddenFlag;
+    }
+
+    public void setHiddenFlag(boolean hiddenFlag) {
+        this.hiddenFlag = hiddenFlag;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+}
--- app/src/main/java/com/example/rello/create_eventActivity.java
+++ app/src/main/java/com/example/rello/create_eventActivity.java
@@ -2,13 +2,46 @@ package com.example.rello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class create_eventActivity extends AppCompatActivity {
-
+    private EditText name;
+    private EditText description;
+    private Switch hiddenFlag;
+    private EditText eventDate;
+    private Button createEvent;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        name = findViewById(R.id.editText2);
+        eventDate = findViewById(R.id.editText7);
+        description = findViewById(R.id.editText8);
+        hiddenFlag = findViewById(R.id.switch1);
+        createEvent = findViewById(R.id.button2);
+        db = FirebaseFirestore.getInstance();
+
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameString = name.getText().toString();
+                String eventDateString = eventDate.getText().toString();
+                String descriptionString = description.getText().toString();
+                boolean privEvent = hiddenFlag.isChecked();
+                Event newEvent = new Event(nameString,eventDateString,descriptionString,privEvent);
+                db.collection("events").add(newEvent);
+                startActivity(new Intent(getApplicationContext(), eventsActivity.class));
+            }
+        });
     }
 }
