097dec1e3af3feed3f883b6d31c519a0921a19cf
Dayan <dayanjayasuriya7@gmail.com>
2019-12-04 19:00:34 -0700

    Create Account Working

--- app/src/main/java/com/example/rello/create_account_Activity.java
+++ app/src/main/java/com/example/rello/create_account_Activity.java
@@ -1,41 +1,29 @@
 package com.example.rello;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rello.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class create_account_Activity extends AppCompatActivity {
+    EditText email,password;
+    Button registerButton,loginButton;
+    FirebaseAuth firebaseAuth;
 
-    Button Btn_next, Btn_login;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account_);
-
-        Btn_next = findViewById(R.id.continue_btn);
-        Btn_login = findViewById(R.id.login_btn);
-        Btn_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNext();
-            }
-        });
-        Btn_login.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLogin();
-            }
-        }));
-
-        //Do something with the user information here
-
-    }
     public void openNext(){
         Intent intent = new Intent(this, create_account_2_Activity.class);
         startActivity(intent);
@@ -43,5 +31,62 @@ public class create_account_Activity extends AppCompatActivity {
     public  void openLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_);
+
+        email = (EditText) findViewById(R.id.email_et);
+        password = (EditText) findViewById(R.id.password_et);
+        registerButton = (Button) findViewById(R.id.continue_btn);
+        loginButton = (Button) findViewById(R.id.login_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if(TextUtils.isEmpty(emailString)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(passwordString)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                }
+
+                if(passwordString.length()<6){
+                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(emailString,passwordString)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(),create_account_2_Activity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(),"E-mail or password is wrong",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),create_account_2_Activity.class));
+        }
     }
 }
